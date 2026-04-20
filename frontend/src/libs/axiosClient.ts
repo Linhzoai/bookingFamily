0dcf9e4ef0a43c9ae5aa3c6504ba09213ab11c26
Linh Nguyen <linhbarao@gmail.com>
2026-04-20 22:19:51 +0700

    fix baseurl

--- frontend/src/libs/axiosClient.ts
+++ frontend/src/libs/axiosClient.ts
@@ -9,7 +9,7 @@ let failedQueue: Array<{resolve: (token: string) => void, reject: (err?: any) =>
 const processQueue= (error: any, token: string|null = null)=>{
     failedQueue.forEach(prom =>{
         if(error) prom.reject(error);
-        else prom.resolve(token);
+        else prom.resolve(token as string);
     })
     failedQueue = [];
 }
